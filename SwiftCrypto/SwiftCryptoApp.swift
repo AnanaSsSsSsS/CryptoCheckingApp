edce5f33106a2e05319cbcc18b3b23d61bacccd1
AnanaSsSsSsS <elias.gold@bk.ru>
2022-10-23 20:16:28 +0300

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by macbook on 23.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
